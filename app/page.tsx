57eb66792337ba798b6ccac8d4d5c5713f9528ac
Manujdixit <manujdixit04@gmail.com>
2025-02-23 15:06:41 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -142,12 +142,12 @@ export default function Page() {
       <div className="relative z-10 flex flex-col min-h-screen">
         {/* Navigation */}
         <nav className="max-w-[490px] h-12 mt-[45px] mx-auto px-4 py-6 flex items-center justify-between bg-[#2e2e2e] border-[0.5px] border-white/10 rounded-[20px]">
-          <div className="flex items-center gap-8 ">
+          <div className="flex items-center gap-4 ">
             <Link href="/" className="text-white flex items-center ">
               <span className="text-2xl">✧</span>
               <span className="ml-2">comet</span>
             </Link>
-            <div className="hidden md:flex gap-6">
+            <div className="hidden md:flex gap-4">
               <Link
                 href="/manifesto"
                 className="text-white hover:text-white/80"
@@ -159,14 +159,15 @@ export default function Page() {
               </Link>
             </div>
           </div>
-          <div className="flex items-center gap-2 ml-2">
+          <div className="flex items-center gap-2 ml-5">
             <Button
+            size={"sm"}
               variant="outline"
               className="text-white hover:text-white/80 bg-transparent hover:text-black"
             >
               Login
             </Button>
-            <Button className="bg-[#f4f1e8] text-black hover:bg-[#f4f1e8]/90">
+            <Button size={"sm"} className="bg-[#f4f1e8] text-black hover:bg-[#f4f1e8]/90">
               Signup
             </Button>
           </div>
